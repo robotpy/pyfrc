446876e7329051591e14c9f80268f65f54fc3618
Dustin Spicuzza <dustin@virtualroadside.com>
2012-12-07 17:17:24 -0500

    Not sure how I forgot these...

--- README.txt
+++ README.txt
@@ -67,10 +67,24 @@ robot.py:
 
 
 Writing test programs
-=====================
+---------------------
 
 All test programs should live in the test modules directory, which is 
 specified by the --test-modules argument to run_test.py
 
 See 'samples/import_test.py' for an example test program that starts the
 robot code and runs it through autonomous mode and operator mode. 
+
+SmartDashboard support
+----------------------
+
+fake-wpilib has a fully working implementation of SmartDashboard, which is
+identical to the implementation in RobotPy that runs on the cRio. Using 
+recent versions of SmartDashboard, you can connect to the test program 
+running on your PC using the following command:
+
+	C:\Program Files\SmartDashboard\SmartDashboard.jar ip 127.0.0.1
+
+Currently, this is only supported in the SVN version of SmartDashboard, and
+does not work in the binaries currently released by FRC. This should work
+in 2013 releases, however.
--- fake_wpilib/SmartDashboard.py
+++ fake_wpilib/SmartDashboard.py
@@ -1,54 +1,149 @@
+import logging
+from .NetworkTables import NetworkTable
+import time
+from .core import PIDController, Gyro
 
-from .pid_controller import PIDController
+class SmartDashboardData:
+    def GetType(self):
+        raise NotImplementedError
+    def GetTable(self):
+        raise NotImplementedError
 
-class SmartDashboard(object):
+class SmartDashboardNamedData(SmartDashboardData):
+    def GetName(self):
+        raise NotImplementedError
 
-    instance = None
-    
+class SmartDashboard:
+    """The {@link SmartDashboard} class is the bridge between robot programs
+    and the SmartDashboard on the laptop.
+
+    When a value is put into the SmartDashboard here, it pops up on the
+    SmartDashboard on the laptop.  Users can put values into and get values
+    from the SmartDashboard."""
     @staticmethod
     def GetInstance():
-        if SmartDashboard.instance is None:
-            SmartDashboard.instance = SmartDashboard()
-        return SmartDashboard.instance
-        
-    def __init__(self):
-        self.data = {}
-        
-    def GetBoolean(self, key):
-        if key in self.data:
-            return self.data[key]
-        return None
-        
-    def GetData(self, key):
-        if key in self.data:
-            return self.data[key]
-        return None
+        try:
+            return SmartDashboard._instance
+        except AttributeError:
+            SmartDashboard._instance = SmartDashboard._inner()
+            return SmartDashboard._instance
 
-    def GetDouble(self,key):
-        if key in self.data:
-            return self.data[key]
-        return None
-        
-    def GetInt(self, key):
-        if key in self.data:
-            return self.data[key]
-        return None
-        
-    def PutBoolean(self, key, value):
-        self.data[key] = bool(value)
-        
-    def PutDouble(self, key, value):
-        self.data[key] = value
-        
-    def PutData(self, key, value):
-        self.data[key] = value
-        
-    def PutInt(self, key, value):
-        self.data[key] = int(value)
-        
-    
-    
-    
+    class _inner:
+        def __init__(self):
+            self.table = NetworkTable.GetTable("SmartDashboard")
+            self.tablesToData = {}
+
+        def __setitem__(self, keyName, value):
+            if hasattr(value, "GetType") and hasattr(value, "GetTable"):
+                # Assumed to be SmartDashboardData
+                self.PutData(keyName, value)
+            else:
+                self.table[keyName] = value
+
+        def __getitem__(self, keyName):
+            v = self.table[keyName]
+            if isinstance(v, NetworkTable):
+                data = self.tablesToData.get(v)
+                if data is None:
+                    logging.error("SmartDashboardMissingKey: %s", keyName)
+                return data
+            else:
+                return v
+
+        def PutData(self, keyName=None, value=None):
+            """Maps the specified key to the specified value in this table.
+            The key can not be None.
+            The value can be retrieved by calling the get method with a key
+            that is equal to the original key.
+            @param keyName the key
+            @param value the value"""
+            if keyName is None and hasattr(value, "GetName"):
+                keyName = value.GetName()
+            if keyName is None:
+                logging.error("NullParameter: keyName")
+                return
+            if value is None:
+                logging.error("NullParameter: value")
+                return
+            t = NetworkTable()
+            t.PutString("~TYPE~", value.GetType())
+            t.PutSubTable("Data", value.GetTable())
+            self.table[keyName] = t
+            self.tablesToData[t] = value
+
+        def GetData(self, keyName):
+            """Returns the value at the specified key.
+            @param keyName the key
+            @return the value"""
+            if keyName is None:
+                logging.error("NullParameter: keyName")
+                return None
+            subtable = self.table.GetSubTable(keyName)
+            data = self.tablesToData.get(subtable)
+            if data is None:
+                logging.error("SmartDashboardMissingKey: %s", keyName)
+            return data
+
+        def PutBoolean(self, keyName, value):
+            """Maps the specified key to the specified value in this table.
+            The key can not be NULL.
+            The value can be retrieved by calling the get method with a key
+            that is equal to the original key.
+            @param keyName the key
+            @param value the value"""
+            self.table.PutBoolean(keyName, value)
+
+        def GetBoolean(self, keyName):
+            """Returns the value at the specified key.
+            @param keyName the key
+            @return the value"""
+            return self.table.GetBoolean(keyName)
+
+        def PutInt(self, keyName, value):
+            """Maps the specified key to the specified value in this table.
+            The keyName can not be None.
+            The value can be retrieved by calling the get method with a key
+            that is equal to the original key.
+            @param keyName the key
+            @param value the value"""
+            self.table.PutInt(keyName, value)
+
+        def GetInt(self, keyName):
+            """Returns the value at the specified key.
+            @param keyName the key
+            @return the value"""
+            return self.table.GetInt(keyName)
+
+        def PutDouble(self, keyName, value):
+            """Maps the specified key to the specified value in this table.
+            The key can not be None.
+            The value can be retrieved by calling the get method with a key
+            that is equal to the original key.
+            @param keyName the key
+            @param value the value"""
+            self.table.PutDouble(keyName, value)
+
+        def GetDouble(self, keyName):
+            """Returns the value at the specified key.
+            @param keyName the key
+            @return the value"""
+            return self.table.GetDouble(keyName)
+
+        def PutString(self, keyName, value):
+            """Maps the specified key to the specified value in this table.
+            Neither the key nor the value can be None.
+            The value can be retrieved by calling the get method with a key
+            that is equal to the original key.
+            @param keyName the key
+            @param value the value"""
+            self.table.PutString(keyName, value)
+
+        def GetString(self, keyName):
+            """Returns the value at the specified key.
+            @param keyName the key
+            @return the value"""
+            return self.table.GetString(keyName)
+
 class SendablePIDController(PIDController):
     """A {@link SendablePIDController} is a {@link PIDController} that can be
     sent over to the {@link SmartDashboard} using the
@@ -114,8 +209,130 @@ class SendablePIDController(PIDController):
 
         if self.table is not None:
             self.table[self.kEnabled] = False
-            
-            
+
+    #
+    # SmartDashboardData interface
+    #
+    def GetType(self):
+        return "PIDController"
+
+    def GetTable(self):
+        if self.table is None:
+            self.table = NetworkTable()
+
+            self.table[self.kP] = self.GetP()
+            self.table[self.kI] = self.GetI()
+            self.table[self.kD] = self.GetD()
+            self.table[self.kSetpoint] = self.GetSetpoint()
+            self.table[self.kEnabled] = self.IsEnabled()
+
+            self.table.AddChangeListenerAny(self)
+        return self.table
+
+    #
+    # NetworkTableChangeListener interface
+    #
+    def ValueChanged(self, table, name, type):
+        if name == self.kP or name == self.kI or name == self.kD:
+            super().SetPID(table.GetDouble(self.kP), table.GetDouble(self.kI),
+                    table.GetDouble(self.kD))
+        elif name == self.kSetpoint:
+            super().SetSetpoint(table.GetDouble(self.kSetpoint))
+        elif name == self.kEnabled:
+            if table.GetBoolean(self.kEnabled):
+                super().Enable()
+            else:
+                super().Disable()
+
+    def ValueConfirmed(self, table, name, type):
+        pass
+
+class SendableGyro(Gyro):
+    """The {@link SendableGyro} class behaves exactly the same as a
+    {@link Gyro} except that it also implements {@link SmartDashboardData} so
+    that it can be sent over to the {@link SmartDashboard}."""
+    # The time (in seconds) between updates to the table
+    kDefaultTimeBetweenUpdates = 0.2
+    kAngle = "angle"
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.offset = 0.0
+        self.period = self.kDefaultTimeBetweenUpdates
+        self.table = None
+        self.publisher = threading.Thread("SendableGyroPublisher",
+                self._PublishTaskRun)
+        self.runPublisher = False
+
+    def __del__(self):
+        if self.table is not None:
+            # Stop the task
+            self.runPublisher = False
+            self.publisher.join()
+
+    def GetAngle(self):
+        return self.offset + super().GetAngle()
+
+    def Reset(self):
+        self.offset = 0.0
+        super().Reset()
+
+    def SetUpdatePeriod(self, period):
+        """Sets the time (in seconds) between updates to the
+        {@link SmartDashboard}.  The default is 0.2 seconds.
+        @param period the new time between updates"""
+        if period <= 0.0:
+            logging.error("ParameterOutOfRange: period <= 0.0")
+        else:
+            self.period = period
+
+    def GetUpdatePeriod(self):
+        """Returns the period (in seconds) between updates to the
+        {@link SmartDashboard}.  This value is independent of whether or not
+        this {@link SendableGyro} is connected to the {@link SmartDashboard}.
+        The default value is 0.2 seconds.
+        @return the period (in seconds)"""
+        return self.period
+
+    def ResetToAngle(self, angle):
+        """Reset the gyro.
+        Resets the gyro to the given heading. This can be used if there is
+        significant drift in the gyro and it needs to be recalibrated after
+        it has been running.
+        @param angle the angle the gyro should believe it is pointing"""
+        self.offset = angle
+        super().Reset()
+
+    #
+    # SmartDashboardData interface
+    #
+    def GetType(self):
+        return "Gyro"
+
+    def GetTable(self):
+        if self.table is None:
+            self.table = NetworkTable()
+            self.table[self.kAngle] = int(GetAngle())
+            self.table.AddChangeListener(self.kAngle, self)
+            self.publisher.start()
+        return self.table
+
+    #
+    # NetworkTableChangeListener interface
+    #
+    def ValueChanged(self, table, name, type):
+        # Update value from smart dashboard
+        self.ResetToAngle(self.table.GetDouble(name))
+
+    def ValueConfirmed(self, table, name, type):
+        pass
+
+    def _PublishTaskRun(self):
+        self.runPublisher = True
+        while self.runPublisher:
+            self.table[self.kAngle] = int(GetAngle())
+            time.sleep(self.period)
+
 class SendableChooser:
     """The {@link SendableChooser} class is a useful tool for presenting a
     selection of options to the {@link SmartDashboard}.
@@ -136,8 +353,7 @@ class SendableChooser:
         self.defaultChoice = None
         self.choices = {}   # map from names to objects
         self.ids = {}       # map from objects to ids
-        #self.table = NetworkTable()
-        self.table = {}
+        self.table = NetworkTable()
         self.count = 0
 
     def AddObject(self, name, obj):
