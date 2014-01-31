446f95a2acedbe3691f286e2f77ac15b817f5357
Dustin Spicuzza <dustin@virtualroadside.com>
2014-01-30 22:30:35 -0500

    Add tooltip object

--- lib/pyfrc/sim/ui_widgets.py
+++ lib/pyfrc/sim/ui_widgets.py
@@ -133,3 +133,54 @@ class PanelIndicator(tk.Frame):
     def set_disabled(self):
         self.canvas.itemconfig(self.light, fill='#aaaaaa')
 
+#
+# from http://www.voidspace.org.uk/python/weblog/arch_d7_2006_07_01.shtml#e387
+#
+
+class Tooltip(object):
+
+    def __init__(self, widget):
+        self.widget = widget
+        self.tipwindow = None
+        self.id = None
+        self.x = self.y = 0
+
+    def showtip(self, text):
+        "Display text in tooltip window"
+        self.text = text
+        if self.tipwindow or not self.text:
+            return
+        x, y, cx, cy = self.widget.bbox("insert")
+        x = x + self.widget.winfo_rootx() + 27
+        y = y + cy + self.widget.winfo_rooty() +27
+        self.tipwindow = tw = tk.Toplevel(self.widget)
+        tw.wm_overrideredirect(1)
+        tw.wm_geometry("+%d+%d" % (x, y))
+        try:
+            # For Mac OS
+            tw.tk.call("::tk::unsupported::MacWindowStyle",
+                       "style", tw._w,
+                       "help", "noActivates")
+        except tk.TclError:
+            pass
+        label = tk.Label(tw, text=self.text, justify=tk.LEFT,
+                         background="#ffffe0", relief=tk.SOLID, borderwidth=1,
+                         font=("tahoma", "8", "normal"))
+        label.pack(ipadx=1)
+
+    def hidetip(self):
+        tw = self.tipwindow
+        self.tipwindow = None
+        if tw:
+            tw.destroy()
+            
+    @staticmethod
+    def create(widget, text):
+        toolTip = Tooltip(widget)
+        def enter(event):
+            toolTip.showtip(text)
+        def leave(event):
+            toolTip.hidetip()
+        widget.bind('<Enter>', enter)
+        widget.bind('<Leave>', leave)
+
