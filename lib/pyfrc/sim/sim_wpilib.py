844b98c4455b3c10bc92d173ca50aa8e5cd5eb4b
Dustin Spicuzza <dustin@virtualroadside.com>
2014-02-09 22:28:28 -0500

    Actually enable the Watchdog monitor class

--- lib/pyfrc/sim/sim_wpilib.py
+++ lib/pyfrc/sim/sim_wpilib.py
@@ -8,7 +8,7 @@ import time
 import threading
 
 from .. import wpilib
-from ..wpilib._wpilib import _fake_time
+from ..wpilib._wpilib import _core, _fake_time
 
 
 class FakeRealTime(object):
@@ -60,113 +60,128 @@ class RealFakeWatchdog(object):
     '''
     
     kDefaultWatchdogExpiration = wpilib.Watchdog.kDefaultWatchdogExpiration
+    
+    @staticmethod
+    def _reset():
+        if hasattr(RealFakeWatchdog, '_instance'):
+            delattr(RealFakeWatchdog, '_instance')
+    
+    @staticmethod
+    def GetInstance():
+        try:
+            return RealFakeWatchdog._instance
+        except AttributeError:
+            RealFakeWatchdog._instance = RealFakeWatchdog._inner()
+            return RealFakeWatchdog._instance
+    
+    class _inner:
 
-    def __init__(self):
-    
-        # a callable object that takes a single parameter, which is the 
-        # time that the watchdog was last fed
-        # -> Note that this handler may be called multiple times, and
-        #    it will be called from the watchdog thread.
-        self.error_handler = None
-    
-        self.enabled = False
-        self.fed = None
-        self.alive = True
-        self.expiration = RealFakeWatchdog.kDefaultWatchdogExpiration
+        def __init__(self):
         
-        self.lock = threading.RLock()
-        self.cv = threading.Condition(self.lock)
+            # a callable object that takes a single parameter, which is the 
+            # time that the watchdog was last fed
+            # -> Note that this handler may be called multiple times, and
+            #    it will be called from the watchdog thread.
+            self.error_handler = None
         
-        self.thread = threading.Thread(target=self._watcher_fn)
-        self.thread.daemon = True
-        self.thread.start()
-        
-    def Feed(self):
-        with self.lock:
-            if not self.enabled:
-                return False
-                
-            was_alive = self.alive
+            self.enabled = False
+            self.fed = None
             self.alive = True
-            self.fed = time.time()
-            self.cv.notify()
-        
-        return was_alive
+            self.expiration = RealFakeWatchdog.kDefaultWatchdogExpiration
             
-    def GetEnabled(self):
-        with self.lock:
-            return self.enabled
-    
-    def GetExpiration(self):
-        with self.lock:
-            raise self.expiration
-        
-    def GetTimer(self):
-        with self.lock:
-            return time.time() - self.fed
+            self.lock = threading.RLock()
+            self.cv = threading.Condition(self.lock)
             
-    def IsAlive(self):
-        with self.lock:
-            return self.alive
-        
-    def IsSystemActive(self):
-        return self.IsAlive()
-        
-    def Kill(self):
-        raise NotImplementedError()
-    
-    def SetEnabled(self, enabled):
-        with self.lock:
-            self.enabled = bool(enabled)
-            self.alive = True
-            self.fed = time.time()
-            self.cv.notify()
-    
-    def SetExpiration(self, expiration):
-        with self.lock:
-            self.expiration = float(expiration)
-            if self.enabled:
+            self.thread = threading.Thread(target=self._watcher_fn)
+            self.thread.daemon = True
+            self.thread.start()
+            
+        def Feed(self):
+            with self.lock:
+                if not self.enabled:
+                    return False
+                    
+                was_alive = self.alive
                 self.alive = True
                 self.fed = time.time()
-            self.cv.notify()
-    
-    def _watcher_fn(self):
-        '''
-            This function runs in a separate thread, and checks the
-            watchdog and determines whether it has starved or not
-        '''
-        
-        with self.lock:
-            while True:
+                self.cv.notify()
             
-                # use the condition variable to wait for the dog to be enabled
-                # -> note that condition variables release the lock while 
-                #    they are waiting to be notified of a change
-                self.cv.wait_for(lambda: self.enabled)
+            return was_alive
                 
-                # once enabled, ensure that the dog is being fed on time
+        def GetEnabled(self):
+            with self.lock:
+                return self.enabled
+        
+        def GetExpiration(self):
+            with self.lock:
+                raise self.expiration
+            
+        def GetTimer(self):
+            with self.lock:
+                return time.time() - self.fed
+                
+        def IsAlive(self):
+            with self.lock:
+                return self.alive
+            
+        def IsSystemActive(self):
+            return self.IsAlive()
+            
+        def Kill(self):
+            raise NotImplementedError()
+        
+        def SetEnabled(self, enabled):
+            with self.lock:
+                self.enabled = bool(enabled)
+                self.alive = True
+                self.fed = time.time()
+                self.cv.notify()
+        
+        def SetExpiration(self, expiration):
+            with self.lock:
+                self.expiration = float(expiration)
+                if self.enabled:
+                    self.alive = True
+                    self.fed = time.time()
+                self.cv.notify()
+        
+        def _watcher_fn(self):
+            '''
+                This function runs in a separate thread, and checks the
+                watchdog and determines whether it has starved or not
+            '''
+            
+            with self.lock:
                 while True:
+                
+                    # use the condition variable to wait for the dog to be enabled
+                    # -> note that condition variables release the lock while 
+                    #    they are waiting to be notified of a change
+                    self.cv.wait_for(lambda: self.enabled)
                     
-                    self.cv.wait(timeout=self.expiration)
-                    
-                    # did someone disable us?
-                    if not self.enabled:
-                        break
-                    
-                    # check for hunger
-                    now = time.time()
-                    period = now - self.fed
-                    
-                    if period > self.expiration:
-                        # looks like someone forgot to feed the watchdog!
-                        self.alive = False
+                    # once enabled, ensure that the dog is being fed on time
+                    while True:
                         
-                        if self.error_handler is not None:
-                            self.error_handler(self.fed, period, self.expiration)
-                        else:                    
-                            print('WATCHDOG FAILURE! Last fed %0.3f seconds ago (expiration: %0.3f seconds)' % 
-                                  (period, self.expiration), file=sys.stderr)
+                        self.cv.wait(timeout=self.expiration)
+                        
+                        # did someone disable us?
+                        if not self.enabled:
+                            break
+                        
+                        # check for hunger
+                        now = time.time()
+                        period = now - self.fed
+                        
+                        if period > self.expiration:
+                            # looks like someone forgot to feed the watchdog!
+                            self.alive = False
+                            
+                            if self.error_handler is not None:
+                                self.error_handler(self.fed, period, self.expiration)
+                            else:                    
+                                print('WATCHDOG FAILURE! Last fed %0.3f seconds ago (expiration: %0.3f seconds)' % 
+                                      (period, self.expiration), file=sys.stderr)
 
-wpilib.Watchdog = RealFakeWatchdog
+_core.Watchdog = RealFakeWatchdog
 
 
