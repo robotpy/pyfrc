13f992c43c5a3fa913704bdb9a3da2bc5091ffb2
Dustin Spicuzza <dustin@virtualroadside.com>
2014-04-01 00:20:18 -0400

    Better error message if coverage isn't installed

--- lib/pyfrc/cli/cli_coverage.py
+++ lib/pyfrc/cli/cli_coverage.py
@@ -6,6 +6,13 @@ import sys
 
 def run(run_fn, file_location):
 
+    try:
+        import coverage
+    except ImportError:
+        print("Error importing coverage module for code coverage testing, did you install it?\n" + 
+              "You can download it at https://pypi.python.org/pypi/coverage\n", file=sys.stderr)
+        return 1
+
     # construct the arguments to run coverage
     args = [sys.executable, '-m', 'coverage',
                             'run', '--source', dirname(file_location),
