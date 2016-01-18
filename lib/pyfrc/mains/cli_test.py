a7ba97a740d6ecb049f81ca7e1fd416ef3ccad66
Dustin Spicuzza <dustin@virtualroadside.com>
2016-01-18 00:21:23 -0500

    Testing improvements (fixes #43)
    - Detect if no tests were collected and warn the user appropriately
    - If there are no tests, ask the user if they would like to add builtin tests

--- lib/pyfrc/mains/cli_test.py
+++ lib/pyfrc/mains/cli_test.py
@@ -1,6 +1,7 @@
 
 import os
 import inspect
+import sys
 
 from os.path import abspath, dirname, exists, join
 
@@ -10,6 +11,9 @@ from ..test_support import pytest_plugin
 
 # TODO: setting the plugins so that the end user can invoke py.test directly
 # could be a useful thing. Will have to consider that later.
+
+class _TryAgain(Exception):
+    pass
 
 #
 # main test class
@@ -31,22 +35,29 @@ class PyFrcTest:
     def run(self, options, robot_class, **static_options):
         # wrapper around run_test that sets the appropriate mode
         
+        
         from .. import config
         config.mode = 'test'
         config.coverage_mode = options.coverage_mode
         
         return self.run_test(options.pytest_args, robot_class, options.builtin, **static_options)
         
-    def run_test(self, pytest_args, robot_class, use_builtin, **static_options):
+    def run_test(self, *a, **k):
+        try:
+            return self._run_test(*a, **k)
+        except _TryAgain:
+            return self._run_test(*a, **k)
+        
+    def _run_test(self, pytest_args, robot_class, use_builtin, **static_options):
     
         # find test directory, change current directory so py.test can find the tests
         # -> assume that tests reside in tests or ../tests
         
         curdir = abspath(os.getcwd())
         
+        self.robot_class = robot_class
         robot_file = abspath(inspect.getfile(robot_class))
         robot_path = dirname(robot_file)
-        test_directory = None
         
         if robot_file.endswith('cProfile.py'):
             # so, the module for the robot class is __main__, and __main__ is
@@ -58,36 +69,67 @@ class PyFrcTest:
                 "ERROR: Cannot run profiling from a directory that does not contain robot.py"
                 return 1
         
-        try_dirs = [join(robot_path, 'tests'), abspath(join(robot_path, '..', 'tests'))]
+        self.try_dirs = [
+            abspath(join(robot_path, 'tests')),
+            abspath(join(robot_path, '..', 'tests'))
+        ]
         
-        for d in try_dirs:
+        for d in self.try_dirs:
             if exists(d):
                 test_directory = d
+                os.chdir(test_directory)
                 break
-        
-        if test_directory is None:
+        else:
             if not use_builtin:
                 print("ERROR: Cannot run robot tests, as test directory was not found!")
-                print()
-                print("Looked for tests at:")
-                for d in try_dirs:
-                    print('- %s' % d)
-                print()
-                print("If you don't want to write your own tests, use the --builtin option to run")
-                print("generic tests that should work on any robot.")
+                retv = self._no_tests()
                 return 1
             
             from ..tests import basic
             pytest_args.insert(0, abspath(inspect.getfile(basic)))
-        else:
-            os.chdir(test_directory)
         
         try:
-            return pytest.main(pytest_args,
+            retv = pytest.main(pytest_args,
                                plugins=[pytest_plugin.PyFrcPlugin(robot_class,
                                                                   robot_file,
                                                                   robot_path)])
         finally:
             os.chdir(curdir)
 
-
+        # requires pytest 2.8.x
+        if retv == 5:
+            print()
+            print("ERROR: a tests directory was found, but no tests were defined")
+            retv = self._no_tests(retv)
+            
+        return retv
+    
+    def _no_tests(self, r=1):
+        print()
+        print("Looked for tests at:")
+        for d in self.try_dirs:
+            print('- %s' % d)
+        print()
+        print("If you don't want to write your own tests, pyfrc comes with generic tests")
+        print("that can test basic functionality of most robots. You can run them by")
+        print("specifying the --builtin option.")
+        print()
+        
+        if not sys.stdin.isatty():
+            print("Alternatively, to create a tests directory with the builtin tests, you can run:")
+            print()
+            print("    python3 robot.py add-tests")
+            print()
+        else:
+            a = ''
+            while a not in ['y', 'n']:
+                a = input("Create a tests directory with builtin tests now? [y/n]").lower()
+                
+            if a == 'y':
+                from .cli_add_tests import PyFrcAddTests
+                add_tests = PyFrcAddTests()
+                add_tests.run(None, self.robot_class)
+                
+                raise _TryAgain()
+            
+        return r
