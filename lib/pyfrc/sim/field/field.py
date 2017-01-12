f341e64c00bb65a8ab54c798c30172148613ccde
Tim Winters <twinters@wpi.edu>
2017-01-12 01:01:10 -0500

    Add field for 2017
    Users can now load in there own image by setting 'pyfrc.field.image' to the name of the image file within the sim directory. If no image is specified, a defaultwill be used.

--- lib/pyfrc/sim/field/field.py
+++ lib/pyfrc/sim/field/field.py
@@ -1,80 +1,81 @@
 
+from os.path import abspath, dirname, join
+from tkinter import PhotoImage
+
 import tkinter as tk
 
 from .elements import DrawableElement
 
+
 class RobotField(object):
-    
+
     def __init__(self, root, manager, config_obj):
         '''
-            initializes all default values and creates 
+            initializes all default values and creates
             a board, waits for run() to be called
             to start the board
-            
+
             manager - sim manager class instance
             board_size - a tuple with values (rows, cols)
         '''
-        
+
         # TODO: support drawing an actual field?
-        
+
         self.manager = manager
         self.elements = []      # robots, walls, missles, etc
-        
+
         field_size = config_obj['pyfrc']['field']['w'], \
-                     config_obj['pyfrc']['field']['h']
+            config_obj['pyfrc']['field']['h']
         px_per_ft = config_obj['pyfrc']['field']['px_per_ft']
-        
+
         # setup board characteristics -- cell size is 1ft
         self.cols, self.rows = field_size
         self.margin = 5
         self.cellSize = px_per_ft
-        self.canvasWidth = 2*self.margin + self.cols*self.cellSize
-        self.canvasHeight = 2*self.margin + self.rows*self.cellSize
-        
+        self.canvasWidth = 2 * self.margin + 485
+        self.canvasHeight = 2 * self.margin + 535
+
         self.canvas = tk.Canvas(root, width=self.canvasWidth, height=self.canvasHeight)
         self.canvas.bind("<Key>", self.on_key_pressed)
         self.canvas.bind("<Button-1>", self.on_click)
-        
+
         self.text_id = None
-        
+
         # Draw the field initially
         self.draw_field()
-        
+
         # Load elements from the config
         # -> This probably belongs somewhere else
-        self._load_field_elements(px_per_ft, config_obj['pyfrc']['field']['objects'])
-        
-    def _load_field_elements(self, px_per_ft, objects):
-        
-        for obj in objects:
-            
-            color = obj['color']
-            pts = [(self.margin + int(pt[0]*px_per_ft),
-                    self.margin + int(pt[1]*px_per_ft)) for pt in obj['points']]
-            element = DrawableElement(pts, None, None, color)
-            self.add_moving_element(element)
-        
-        
+        self._load_field_elements(px_per_ft, config_obj)
+
+    def _load_field_elements(self, px_per_ft, config_obj):
+        image = config_obj['pyfrc']['field']['image']
+        if image is None:
+            self.photo = PhotoImage(file=abspath(join(dirname(__file__), 'field.gif')))
+        else:
+            self.photo = PhotoImage(file=join(config_obj['simpath'], image))
+        self.canvas.create_image((self.canvasWidth / 2, self.canvasHeight / 2), image=self.photo)
+
     def add_moving_element(self, element):
         '''Add elements to the board'''
-        
+
         element.initialize(self.canvas)
         self.elements.append(element)
-    
+
     def grid(self, *args, **kwargs):
         self.canvas.grid(*args, **kwargs)
-        
+
     def on_key_pressed(self, event):
         '''
             likely to take in a set of parameters to treat as up, down, left,
             right, likely to actually be based on a joystick event... not sure
             yet
         '''
-        
+
         return
-    
+
         # TODO
-        
+
         if event.keysym == "Up":
             self.manager.set_joystick(0.0, -1.0, 0)
         elif event.keysym == "Down":
@@ -83,29 +84,30 @@ class RobotField(object):
             self.manager.set_joystick(-1.0, 0.0, 0)
         elif event.keysym == "Right":
             self.manager.set_joystick(1.0, 0.0, 0)
-            
+
         elif event.char == " ":
             mode = self.manager.get_mode()
             if mode == self.manager.MODE_DISABLED:
                 self.manager.set_mode(self.manager.MODE_OPERATOR_CONTROL)
             else:
                 self.manager.set_mode(self.manager.MODE_DISABLED)
-    
+
     def on_click(self, event):
         self.canvas.focus_set()
-    
+
     def update_widgets(self):
-        
+
         # TODO: process collisions and such too
-        
+
         for element in self.elements:
             element.perform_move()
-            
+
     def draw_field(self):
-        for row in range(self.rows):
-            for col in range(self.cols):
-                self.draw_board_cell(row, col)
-            
+        pass
+        # for row in range(self.rows):
+        #    for col in range(self.cols):
+        #        self.draw_board_cell(row, col)
+
     def draw_board_cell(self, row, col):
         left = self.margin + col * self.cellSize
         right = left + self.cellSize
