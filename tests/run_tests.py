792f68b17fddbca810fbbad6208d4aaf6935fee9
David Vo <auscompgeek@users.noreply.github.com>
2025-01-03 13:05:03 +1100

    Bump pint for Python 3.13 (#237)

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -9,6 +9,4 @@ if __name__ == "__main__":
     root = abspath(dirname(__file__))
     os.chdir(root)
 
-    # TODO: https://github.com/hgrecco/pint/issues/1969
-    if sys.version_info < (3, 13):
-        subprocess.check_call([sys.executable, "-m", "pytest", "-vv"])
+    subprocess.check_call([sys.executable, "-m", "pytest", "-vv"])
